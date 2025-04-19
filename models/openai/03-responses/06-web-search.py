f781a34bdea5280c3a2737ea9add793fd38cf6f3
Abdul Nelfrank <nelfrankjunior@gmail.com>
2025-04-19 18:48:40 +0300

    Add basic web search example with user location support in Responses API using gpt-4.1-mini.

--- models/openai/03-responses/06-web-search.py
+++ models/openai/03-responses/06-web-search.py
@@ -13,3 +13,26 @@ response = client.responses.create(
 )
 
 print(response.output_text)
+
+# --------------------------------
+# Basic web search with location
+# --------------------------------
+
+response = client.responses.create(
+    model="gpt-4.1-mini",
+    tools=[
+        {
+            "type": "web_search_preview",
+            "user_location": {
+                "type": "approximate",
+                "country": "KE",
+                "city": "Nairobi",
+            },
+        }
+    ],
+    input="What are the best restaurant in Ruaka?",
+)
+
+print(response.output_text)
+response.output[1].content[0].annotations
+response.output[1].content[0].annotations[0].url
